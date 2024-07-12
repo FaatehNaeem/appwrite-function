af1f4f46e57cd107f8d0bf13218253ae0f07fb6c
Faateh <141557318+FaatehNaeem@users.noreply.github.com>
2024-07-12 23:54:15 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,33 +1,23 @@
-import { Client } from 'node-appwrite';
+const axios = require('axios');
 
-// This is your Appwrite function
-// It's executed each time we get a request
-export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+module.exports = async function (req, res) {
+    try {
+        const notificationResponse = await axios.post('https://api.nativenotify.com/v1/notifications', {
+            appId: 22441,
+            apiKey: 'PXoDlzlfuW4L0PJ4olFvDV',
+            title: 'Daily Notification',
+            message: 'This is your daily notification!',
+            // Add any other parameters as needed
+        });
 
-  // You can log messages to the console
-  log('Hello, Logs!');
-
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
+        res.json({
+            status: 'success',
+            response: notificationResponse.data
+        });
+    } catch (error) {
+        res.json({
+            status: 'error',
+            message: error.message
+        });
+    }
 };
